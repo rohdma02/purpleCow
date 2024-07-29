2e44c1250832cbfee27ce273d04e9a0eec2ef057
rohdma02 <rohdma02@luther.edu>
2024-07-29 13:46:06 -0500

    front end basic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+from flask import render_template
+
 # App Initialization
 from . import create_app  # from __init__ file
 app = create_app(os.getenv("CONFIG_MODE"))
@@ -8,8 +10,13 @@ app = create_app(os.getenv("CONFIG_MODE"))
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+
+@app.route("/animals/")
+def animals():
+    return render_template("animals.html")
 
 
 if __name__ == "__main__":
